d6d1f263fb1a33545148ca3c11039ef0d798b7d0
Joao Pedro <jp.furini@hotmail.com>
2020-04-06 07:02:56 -0300

    Create and implement projects CRUD

---
+++ gonodeapi/app/Controllers/Http/ProjectController.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const Project = use('App/Models/Project')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with projects
+ */
+class ProjectController {
+
+  async index({ request, response, view }) {
+
+    const projects = await Project.query().with('user').fetch()
+
+    return projects
+
+  }
+
+
+  async store({ request, response, auth }) {
+
+    const data = request.only(['title', 'description'])
+
+    const project = await Project.create({ ...data, user_id: auth.user.id })
+
+    return project
+  }
+
+
+  async show({ params, request, response, view }) {
+
+    const project = await Project.findOrFail(params.id)
+
+    await project.load('user')
+    await project.load('tasks')
+
+    return project
+  }
+
+  async update({ params, request, resposne }) {
+
+    const project = await Project.findOrFail(params.id)
+
+    const data = request.only(['title', 'description'])
+
+    project.merge(data)
+
+    await project.save()
+
+    return project
+  }
+
+  async destroy({ params }) {
+
+    const project = await Project.findOrFail(params.id)
+
+    await project.delete()
+  }
+
+
+
+}
+
+module.exports = ProjectController
