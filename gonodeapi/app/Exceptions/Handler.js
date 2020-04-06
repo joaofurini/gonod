0e8208fd717ff14a30c55d2e7fa692be3af3bcfe
Joao Pedro <jp.furini@hotmail.com>
2020-04-06 09:00:48 -0300

    Create and implement error handler

---
+++ gonodeapi/app/Exceptions/Handler.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const Env = use('Env')
+const Youch = use('youch')
+
+const BaseExceptionHandler = use('BaseExceptionHandler')
+
+/**
+ * This class handles all exceptions thrown during
+ * the HTTP request lifecycle.
+ *
+ * @class ExceptionHandler
+ */
+class ExceptionHandler extends BaseExceptionHandler {
+
+  async handle(error, { request, response }) {
+    if (error.name === 'ValidationException') {
+      return response.status(error.status).send(error.messages)
+    }
+
+    if (Env.get('NODE_ENV') === 'development') {
+
+      const youch = new Youch(error, request.request)
+
+      const errorJson = await youch.toJSON()
+
+      return response.status(error.status).send(errorJson)
+
+    }
+
+    return response.status(error.status)
+  }
+
+  async report(error, { request }) {
+
+    console.log(error)
+  }
+}
+
+module.exports = ExceptionHandler
